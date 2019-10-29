b5d8f8565e1b1c4be61e532720945d1b255010d4
Carl Lindgren <carlindgren@outlook.com>
2019-10-29 14:22:42 +0100

    nytt dokument, prövar en idé

--- Addera Rubriker i en lista/app.js
+++ Addera Rubriker i en lista/app.js
@@ -1,14 +1,15 @@
 const addForm = document.querySelector('.addForm')
 const list = document.querySelector('.list')
-
+let arr = [];
 addForm.addEventListener('submit', (e) => {
-let header = addForm.add.value.trim();
-    console.log(e)
+let input = addForm.add.value.trim(); //tar bort mellanrum
+let header = input.charAt(0).toUpperCase() + input.substring(1) //ändrar så att första bokstaven är stor bokstav 
     e.preventDefault();
 
-    if(header) {
+    if(header) { //om input inte är tom kör if-satsen
     list.innerHTML += `<li> ${header}</li>` //Här ska vi addera rubriken
     }
 addForm.reset()
 });
+//spara i locale storage
 
