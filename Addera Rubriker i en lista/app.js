1af06f8eecab68cdfd9fa03436e056b279e4e64e
Carl Lindgren <carlindgren@outlook.com>
2019-10-30 09:12:15 +0100

    sparas i localeStorage - inget renderas

--- Addera Rubriker i en lista/app.js
+++ Addera Rubriker i en lista/app.js
@@ -1,15 +1,28 @@
 const addForm = document.querySelector('.addForm')
 const list = document.querySelector('.list')
-let arr = [];
+let notes = [];
 addForm.addEventListener('submit', (e) => {
-let input = addForm.add.value.trim(); //tar bort mellanrum
-let header = input.charAt(0).toUpperCase() + input.substring(1) //ändrar så att första bokstaven är stor bokstav 
+
+    let input = addForm.add.value.trim(); //tar bort mellanrum
+    let header = input.charAt(0).toUpperCase() + input.substring(1) //ändrar så att första bokstaven är stor bokstav 
     e.preventDefault();
+
+    const note = {
+        text: header,
+        id: Date.now() // bra med tanke på att vi ska visa när dokumenten skapades.
+    }
 
     if(header) { //om input inte är tom kör if-satsen
     list.innerHTML += `<li> ${header}</li>` //Här ska vi addera rubriken
-    }
+    notes.push(note)
+}
+saveNotes()
 addForm.reset()
 });
-//spara i locale storage
 
+//spara i locale storage
+const saveNotes = () => {
+        localStorage.setItem("notes", JSON.stringify(notes));
+    
+}  
+console.log(localStorage)
