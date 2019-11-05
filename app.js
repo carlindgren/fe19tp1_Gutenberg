09d2c046e08e4f70dec813c0323407078aa19076
Carl Lindgren <carlindgren@outlook.com>
2019-11-05 16:15:17 +0100

    skapar nytt fält att skriva i för varje ny rubrik -- inte riktigt som tänkt.

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const doc = document.querySelector('.document')
 let notes = [];
 
 // tar bort infotexten när localstorage.length blir 1.
-let empty = () => {};
-let displayInfo = () => {
-    if(localStorage.length === 1){   
-    info.outerHTML = '';
-    displayInfo = empty
+//let empty = () => { };
+let undisplayInfo = () => {
+    if (localStorage.length) {
+        info.remove();
+       // displayInfo = empty
     }
 }
 
@@ -18,55 +18,56 @@ const renderEditor = () => {
     doc.firstElementChild.innerHTML = `
     <textarea id="mytextarea" name="mytextarea">Skriv något</textarea>
     `
-    document.localation.reload();
 }
 
 
 addForm.addEventListener('submit', (e) => {
-    
+    undisplayInfo()
     let input = addForm.add.value.trim(); //tar bort mellanrum
     let header = input.charAt(0).toUpperCase() + input.substring(1) //ändrar så att första bokstaven är stor bokstav 
     e.preventDefault();
-    
+
 
     const note = {
         text: header,
         tid: Date.now() // bra med tanke på att vi ska visa när dokumenten skapades.
     }
 
-    if(header) { //om input inte är tom kör if-satsen
+    if (header) { //om input inte är tom kör if-satsen
+        list.innerHTML += `<li> ${header}</li>` //Här ska vi addera rubriken
+        notes.push(note)
+        renderEditor()
+        
+        
+    }
     
-    list.innerHTML += `<li> ${header}</li>` //Här ska vi addera rubriken
-    notes.push(note)
-}
-renderEditor()
-saveNotes()
-displayInfo()
-addForm.reset()
+    saveNotes()
+    
+    addForm.reset()
 });
 
 //spara i locale storage
 const saveNotes = () => {
-        localStorage.setItem('notes', JSON.stringify(notes));
-}  
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
 
 const loadNotes = () => {
-//om null, ger oss tom array. inget att ladda.
-notes = localStorage.getItem('notes') ? JSON.parse(localStorage.getItem("notes")) : [];
-console.log(notes.length) // kollar hur många notes som finns.
-return notes //returnerar notes för att passera in i renderNotes();
+    //om null, ger oss tom array. inget att ladda.
+    notes = localStorage.getItem('notes') ? JSON.parse(localStorage.getItem("notes")) : [];
+    console.log(notes.length) // kollar hur många notes som finns.
+    return notes //returnerar notes för att passera in i renderNotes();
 }
 // loadNotes()
 
 const renderNotes = (notes) => {
     for (let i = 0; i < notes.length; i++) {
-    list.innerHTML += `<li> ${notes[i].text}</li>` //tar alla notes och renderar på skärmen
+        list.innerHTML += `<li> ${notes[i].text}</li>` //tar alla notes och renderar på skärmen
     }
-} 
+}
 // renderNotes(notes)
 
 window.addEventListener('DOMContentLoaded', (event) => {
     loadNotes();
     renderNotes(notes);
-    displayInfo()
+    undisplayInfo()
 });
