b6e2c7b1d4f526ded95d3fa1c5cdebb9cfdf7621
Carl Lindgren <carlindgren@outlook.com>
2019-11-12 14:00:42 +0100

    bläddra bland notes

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const list = document.querySelector('.list');
 const doc = document.querySelector('.document');
 const editorContent = document.querySelector('#editor');
 const saveBtn = document.querySelector('.save')
-let content;
 let notes = [];
+let content;
+let currentId; // bör innehålla ID:t på "aktiv" note
+let currentContent;
 
 // tar bort infotexten när localstorage.length blir 1.
 let hideInfo = () => {
@@ -21,13 +23,7 @@ const renderEditor = () => {
     info.remove();
 }
 
-
-quill.on('text-change', () => {
-  content = quill.getContents()
-
-});
-
-addForm.addEventListener('submit', (e)=> {
+addForm.addEventListener('submit', (e) => {
     hideInfo();
     let input = addForm.add.value.trim(); //tar bort mellanrum
     let text = input.charAt(0).toUpperCase() + input.substring(1); //ändrar så att första bokstaven är stor bokstav 
@@ -36,18 +32,23 @@ addForm.addEventListener('submit', (e)=> {
     const note = {
         text,
         id: Date.now(), // bra med tanke på att vi ska visa när dokumenten skapades.
-        content, content // lägga till ett nytt objekt för att spara documentet och kunna rendera.
+        content // lägga till ett nytt objekt för att spara documentet och kunna rendera.
     }
- 
+
 
     if (text) { //om input inte är tom kör if-satsen
-        list.innerHTML += `<li id="${note.id}">${dateFns.format(note.id, 'dddd Do MMMM YYYY')} ${text}</li>` //Här ska vi addera rubriken
-        notes.push(note) //funkar inte bra!! 
+        //list.innerHTML += `<li id="${note.id}">${dateFns.format(note.id, 'dddd Do MMMM YYYY')} ${text}</li>` 
+        list.innerHTML += renderNote(note);
+        notes.push(note) 
         renderEditor()
     }
+    currentId = notes[notes.length - 1].id; //sätter nuvarande id vid submit.
+    console.log(note.length)
+    currentContent = notes[notes.length - 1].content //sätter nuvarande content vid submit.
+    console.log('currentid ' + currentId)
+    console.log('current content ' + currentContent)
     saveNotes();
     addForm.reset()
-
 });
 
 //spara i local storage
@@ -65,14 +66,42 @@ const loadNotes = () => {
 // loadNotes()
 
 const renderNotes = notes => {
-    for (let i = 0; i < notes.length; i++) {
-        list.innerHTML += `<li> ${dateFns.format(notes[i].id, 'dddd Do MMMM YYYY')} ${notes[i].text}</li>` //tar alla notes och renderar på skärmen
-    }
+    notes.forEach(note => {
+        //list.innerHTML += `<li id="${note.id}"> ${dateFns.format(note.id, 'dddd Do MMMM YYYY')} ${note.text}</li>`
+        list.innerHTML += renderNote(note);
+    })
 }
+//skapar en note, renderar vid onload och submit.
+const renderNote = note => `<li id="${note.id}"> ${dateFns.format(note.id, 'dddd Do MMMM YYYY')} ${note.text}</li>`
+
 // renderNotes(notes)
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log("ran")
-    renderNotes(loadNotes());
+    console.log('ran')
+    notes = loadNotes()
+    renderNotes(notes);
     hideInfo()
 });
+
+quill.on('text-change', () => {
+    currentContent = quill.getContents()
+    console.log(currentContent)
+    if (currentId) { //om vi har ett currentID, leta rätt på nuvarande note.
+        let selectedNote = notes.find(note => note.id == currentId);
+        selectedNote.content = currentContent
+    }
+    saveNotes()
+});
+
+list.addEventListener('click', (e) => {
+
+    // klickade vi på krysset?
+    console.log(e.target)
+    // annars:
+    let selectedNote = notes.find(note => note.id == e.target.id);
+    currentId = selectedNote.id // sätter current id när man klickar på noten
+    console.log(currentContent)
+    currentContent = selectedNote.content; 
+    quill.setContents(selectedNote.content)
+    console.log('current id ' + currentId)
+})
