245c94a7edab082050a04bb73cf6ccdd303fe761
ddilana <dilan.yavuz@student.kyh.se>
2019-11-25 09:23:17 +0100

    navbar/tema

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let currentId; // bör innehålla ID:t på "aktiv" note
 let currentContent;
 var element = document.getElementsByClassName("ql-editor");
 
-let activeNote =  {active: Object}
+let activeNote = { active: Object }
 
 
 const template1 = {
@@ -180,79 +180,79 @@ const template1 = {
       "insert": "\nKlistra in länkar från Pinterest och andra webbplatser eller använd anteckningslänkar för att länka till innehåll du har sparat med Web Clipper.\n"
     }
   ]
-}   
+}
 
 // tar bort infotexten när localstorage.length blir 1.
 let hideInfo = () => {
-    if (localStorage.length > 0) {
-        info.remove();
-        renderEditor();
-    }
+  if (localStorage.length > 0) {
+    info.remove();
+    renderEditor();
+  }
 }
 
 //funktion för att rendera editorn vid submit.
 const renderEditor = () => {
-    doc.classList.remove("hidden");
-    info.remove();
+  doc.classList.remove("hidden");
+  info.remove();
 }
 
 addForm.addEventListener('submit', (e) => {
-    hideInfo();
-    let input = addForm.add.value.trim(); //tar bort mellanrum
-    let text = input.charAt(0).toUpperCase() + input.substring(1); //ändrar så att första bokstaven är stor bokstav 
-    e.preventDefault();
+  hideInfo();
+  let input = addForm.add.value.trim(); //tar bort mellanrum
+  let text = input.charAt(0).toUpperCase() + input.substring(1); //ändrar så att första bokstaven är stor bokstav 
+  e.preventDefault();
 
-    const note = {
-        text,
-        id: Date.now(), // bra med tanke på att vi ska visa när dokumenten skapades.
-        content, // lägga till ett nytt objekt för att spara documentet och kunna rendera.
-        favourite: false, //detta är kvar att göra
-        deleted: false //kvar att göra
-    }
+  const note = {
+    text,
+    id: Date.now(), // bra med tanke på att vi ska visa när dokumenten skapades.
+    content, // lägga till ett nytt objekt för att spara documentet och kunna rendera.
+    favourite: false, //detta är kvar att göra
+    deleted: false //kvar att göra
+  }
 
 
-    if (text) { //om input inte är tom kör if-satsen
-        //list.innerHTML += `<li id="${note.id}">${dateFns.format(note.id, 'dddd Do MMMM YYYY')} ${text}</li>` 
-        list.innerHTML += renderNote(note);
-        notes.push(note) 
-        renderEditor()
-    }
-    currentId = notes[notes.length - 1].id; //sätter nuvarande id vid submit.
-    console.log(note.length)
-    currentContent = notes[notes.length - 1].content //sätter nuvarande content vid submit.
-    console.log('currentid ' + currentId)
-    console.log('current content ' + currentContent)
-    saveNotes();
-    element[0].innerHTML = "";
-    addForm.reset()
+  if (text) { //om input inte är tom kör if-satsen
+    //list.innerHTML += `<li id="${note.id}">${dateFns.format(note.id, 'dddd Do MMMM YYYY')} ${text}</li>` 
+    list.innerHTML += renderNote(note);
+    notes.push(note)
+    renderEditor()
+  }
+  currentId = notes[notes.length - 1].id; //sätter nuvarande id vid submit.
+  console.log(note.length)
+  currentContent = notes[notes.length - 1].content //sätter nuvarande content vid submit.
+  console.log('currentid ' + currentId)
+  console.log('current content ' + currentContent)
+  saveNotes();
+  element[0].innerHTML = "";
+  addForm.reset()
 });
 
 //spara i local storage
 const saveNotes = () => {
-    localStorage.setItem('notes', JSON.stringify(notes));
+  localStorage.setItem('notes', JSON.stringify(notes));
 }
 const saveActiveNote = () => {
-    localStorage.setItem('activeNote', JSON.stringify(activeNote.id))//activeNote    
+  localStorage.setItem('activeNote', JSON.stringify(activeNote.id))//activeNote    
 }
 const loadActiveNote = () => {
-    return localStorage.getItem('activeNote') ? JSON.parse(localStorage.getItem('activeNote')) : 0;
+  return localStorage.getItem('activeNote') ? JSON.parse(localStorage.getItem('activeNote')) : 0;
 
-}   
+}
 const loadNotes = () => {
-    //om null, ger oss tom array. inget att ladda.
+  //om null, ger oss tom array. inget att ladda.
 
-    console.log(notes.length) // kollar hur många notes som finns.
-    return localStorage.getItem('notes') ? JSON.parse(localStorage.getItem("notes")) : []; //returnerar notes för att passera in i renderNotes();
+  console.log(notes.length) // kollar hur många notes som finns.
+  return localStorage.getItem('notes') ? JSON.parse(localStorage.getItem("notes")) : []; //returnerar notes för att passera in i renderNotes();
 }
 // loadNotes()
 
 const renderNotes = notes => {
-    notes.forEach(note => {
-        if(note.deleted === false){
-        //list.innerHTML += `<li id="${note.id}"> ${dateFns.format(note.id, 'dddd Do MMMM YYYY')} ${note.text}</li>`
-        list.innerHTML += renderNote(note);
-      }
-   })
+  notes.forEach(note => {
+    if (note.deleted === false) {
+      //list.innerHTML += `<li id="${note.id}"> ${dateFns.format(note.id, 'dddd Do MMMM YYYY')} ${note.text}</li>`
+      list.innerHTML += renderNote(note);
+    }
+  })
 }
 //skapar en note, renderar vid onload och submit.
 const renderNote = note => `<li id="${note.id}"> ${dateFns.format(note.id, 'dddd Do MMMM YYYY')} ${note.text} <span class="delete">x</span><span class="favourite"><3</span></li>`
@@ -260,51 +260,63 @@ const renderNote = note => `<li id="${note.id}"> ${dateFns.format(note.id, 'dddd
 // renderNotes(notes)
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('ran')
-    notes = loadNotes()
-    renderNotes(notes);
-    let activeNoteID = loadActiveNote();
-    if (activeNoteID > 0) {
+  console.log('ran')
+  notes = loadNotes()
+  renderNotes(notes);
+  let activeNoteID = loadActiveNote();
+  if (activeNoteID > 0) {
     let selectedNote = notes.find(note => note.id == activeNoteID)
     currentId = activeNoteID;
     quill.setContents(selectedNote.content)
-    }
-    hideInfo()
+  }
+  hideInfo()
 });
 
 quill.on('text-change', () => {
-    currentContent = quill.getContents()
-    console.log(currentContent)
-    if (currentId) { //om vi har ett currentID, leta rätt på nuvarande note.
-        let selectedNote = notes.find(note => note.id == currentId);
-        selectedNote.content = quill.getContents();
-        activeNote = selectedNote // lägger in innehållet i active note i vårt nya objekt.
-    }
-    saveNotes()
+  currentContent = quill.getContents()
+  console.log(currentContent)
+  if (currentId) { //om vi har ett currentID, leta rätt på nuvarande note.
+    let selectedNote = notes.find(note => note.id == currentId);
+    selectedNote.content = quill.getContents();
+    activeNote = selectedNote // lägger in innehållet i active note i vårt nya objekt.
+  }
+  saveNotes()
 });
 
 list.addEventListener('click', (e) => {
-//när vi tycker på knappen ska vi leta efter e.target.class = 'delete'
-//om klassen finns ta bort från listan.
-// klickade vi på krysset?
-// annars:
-    let selectedNote = notes.find(note => note.id == e.target.id || note.id == e.target.parentElement.id);
-    if (e.target.className === 'delete'){
-        e.target.parentElement.remove();
-        selectedNote.deleted = true
-    
-    }else if (e.target.className === 'favourite'){
-        selectedNote.favourite = true  
-        e.target.parentElement.
-        console.log(fav.innerHTML);
+  //när vi tycker på knappen ska vi leta efter e.target.class = 'delete'
+  //om klassen finns ta bort från listan.
+  // klickade vi på krysset?
+  // annars:
+  let selectedNote = notes.find(note => note.id == e.target.id || note.id == e.target.parentElement.id);
+  if (e.target.className === 'delete') {
+    e.target.parentElement.remove();
+    selectedNote.deleted = true
 
-    }else
+  } else if (e.target.className === 'favourite') {
+    selectedNote.favourite = true
+    e.target.parentElement.
+      console.log(fav.innerHTML);
+
+  } else
 
     currentId = selectedNote.id // sätter current id när man klickar på noten
-    currentContent = selectedNote.content; 
-    console.log(selectedNote)
-    quill.setContents(selectedNote.content)
-    saveNotes()
-    saveActiveNote();
-    console.log('current id ' + currentId)
+  currentContent = selectedNote.content;
+  console.log(selectedNote)
+  quill.setContents(selectedNote.content)
+  saveNotes()
+  saveActiveNote();
+  console.log('current id ' + currentId)
 })
+
+/* ------DIDI------- */
+
+/* document.querySelector(".fa-search");
+document.querySelector(".search-input").focus();
+document.querySelector(".search-input").focus(); */
+
+
+
+/* document.getElementById("#src").click();
+ */
+
